bf408efab5ca16a57d3a98546556d3025942d2f5
smalaca <ab13.krakow@gmail.com>
2020-02-20 19:57:29 +0100

    cleaned up input file parsing

--- src/main/java/com/allbreakers/AllBreakersApp.java
+++ src/main/java/com/allbreakers/AllBreakersApp.java
@@ -3,12 +3,19 @@ package com.allbreakers;
 import java.io.IOException;
 
 class AllBreakersApp {
+    private static final AlgorithmInputFactory algorithmInputFactory = new AlgorithmInputFactory();
+
     public static void main(String[] args) throws IOException {
-        String fileName = "f_libraries_of_the_world.txt";
-        AlgorithmInputFactory algorithmInputFactory = new AlgorithmInputFactory();
+        process("b_read_on.txt");
+        process("c_incunabula.txt");
+        process("d_tough_choices.txt");
+        process("e_so_many_books.txt");
+        process("f_libraries_of_the_world.txt");
+        process("a_example.txt");
+    }
 
+    private static void process(String fileName) throws IOException {
         AlgorithmInput input = algorithmInputFactory.create(fileName);
-
         System.out.println(input.toString());
     }
 }
