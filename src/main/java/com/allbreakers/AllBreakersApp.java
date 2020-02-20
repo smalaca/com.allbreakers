427eb0e195c9b3f4ffe8a4421f09c89dcff39d10
smalaca <ab13.krakow@gmail.com>
2020-02-20 20:58:50 +0100

    algorithm skeleton with included recognition of end time

--- src/main/java/com/allbreakers/AllBreakersApp.java
+++ src/main/java/com/allbreakers/AllBreakersApp.java
@@ -7,8 +7,9 @@ class AllBreakersApp {
     private static final AlgorithmInputFactory algorithmInputFactory = new AlgorithmInputFactory();
 
     public static void main(String[] args) throws IOException {
-        process("a_example.txt");
-//        process("b_read_on.txt");
+//        process("a_example.txt");
+//        process("a_example.txt");
+        process("b_read_on.txt");
 //        process("c_incunabula.txt");
 //        process("d_tough_choices.txt");
 //        process("e_so_many_books.txt");
@@ -21,7 +22,7 @@ class AllBreakersApp {
         ScanningFacility scanningFacility = input.getScanningFacility();
 
         int availableTime = scanningFacility.getAvailableTime();
-        int currentDay = 0;
+        int currentDay = 1;
 
         fullCycle(libraries, scanningFacility, availableTime, currentDay);
 
@@ -39,21 +40,23 @@ class AllBreakersApp {
         libraries.remove(theBest);
 
         // signup
-        currentDay += theBest.getSignupTimeDays();
-        availableTime -= theBest.getSignupTimeDays();
+        if(theBest != null) {
+            currentDay += theBest.getSignupTimeDays();
+            availableTime -= theBest.getSignupTimeDays();
 
-        // remove all books to not include them again
-        List<Integer> booksThatWillBeScannedTillDeadline = theBest.booksThatWillBeScannedInNext(availableTime - currentDay);
-        scanningFacility.add(booksThatWillBeScannedTillDeadline);
+            // remove all books to not include them again
+            List<Integer> booksThatWillBeScannedTillDeadline = theBest.booksThatWillBeScannedInNext(availableTime - currentDay);
+            scanningFacility.add(booksThatWillBeScannedTillDeadline);
 
-        // remove from all libraries
-        for (Library library : libraries) {
-            library.removeAll(booksThatWillBeScannedTillDeadline);
-        }
+            // remove from all libraries
+            for (Library library : libraries) {
+                library.removeAll(booksThatWillBeScannedTillDeadline);
+            }
 
-        // can we continue
-        if (!libraries.isEmpty() && currentDay != availableTime) {
-            fullCycle(libraries, scanningFacility, availableTime, currentDay);
+            // can we continue
+            if (!libraries.isEmpty() && currentDay != availableTime) {
+                fullCycle(libraries, scanningFacility, availableTime, currentDay);
+            }
         }
     }
 }
